9889e26a3ca2f419c1270b232e87dc4d8c70bebf
Ghanshyam <ghanshyam7595@gmail.com>
2024-07-15 02:55:40 +0530

    controller update

--- src/main/java/com/gstech/Order/theta/controller/OrderController.java
+++ src/main/java/com/gstech/Order/theta/controller/OrderController.java
@@ -6,10 +6,8 @@ import com.gstech.Order.theta.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
-
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
